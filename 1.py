d981d7519674ae546fb9ceef17506694a80a284e
Ivan <vvhappyguy@gmail.com>
2018-05-15 00:56:52 +0300

    TextEditor
    MayBe some changes

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QDesktopWidget, QMessageBox
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, qApp, QAction, QTextEdit
 from PyQt5.QtGui import QIcon
 
 
@@ -9,14 +9,31 @@ class Example(QMainWindow):
     def __init__(self):
         super().__init__() # C-tor of deafult Class
         self.initUI() # UI - C-tor of this Class
+        self.save_status = 0
 
 
     def initUI(self):
-        self.statusBar().showMessage('Ready')
+        # Window block
         self.resize(800,600)
         self.center()
         self.setWindowTitle('DyakinEditor') 
         self.setWindowIcon(QIcon('icon.png'))
+
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+
+        # Manu Block
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)        
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        self.statusBar().showMessage('Ready')
+
+        
+
         self.show()
 
     def center(self):
@@ -32,8 +49,8 @@ class Example(QMainWindow):
         else:
             event.ignore()
 
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = Example()
+    text = ex.textEdit.toPlainText()
     sys.exit(app.exec_())
---
+++ 2.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication
+from PyQt5.QtGui import QIcon
+class Example(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):               
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+        exitAction = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+        self.statusBar()
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+        
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle('Main window')    
+        self.show()
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
